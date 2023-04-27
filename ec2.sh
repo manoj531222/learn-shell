d66c5374c6d7b6c8960ab5943c395b92a927a789
manoj <gandhamanojkutty@gmail.com>
2023-04-27 11:00:30 +0530

    ec2

---
+++ ec2.sh
@@ -0,0 +1,3 @@
+for component in frontend mongodb catalogue ; do
+  aws ec2 run-instances --image-id ami-0b5a2b5b8f2be4ec2 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+done
