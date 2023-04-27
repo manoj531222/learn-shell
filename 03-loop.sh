09053922844768cf238223d4faee5d2ca99dc6fc
manoj <gandhamanojkutty@gmail.com>
2023-04-27 07:34:27 +0530

    loop.sh

--- 03-loop.sh
+++ 03-loop.sh
@@ -3,5 +3,10 @@ a=10
 while [ $a -gt 0 ]; do
   echo a - $a - greter then 0
   a=$(($a-1))
-  sleep 2
+  sleep 1
+  done
+
+for persons names manoj prashanth venkat ; do
+  echo persons names - $persons
+  sleep 1
   done
